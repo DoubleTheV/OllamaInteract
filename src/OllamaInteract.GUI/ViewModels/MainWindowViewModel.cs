afe444d50e547f0cafd9cff046b3fa2dea53e2f3
VV <wilczu102@gmail.com>
2025-10-05 15:31:49 +0000

    Create Initialization logic of the Main Window

--- src/OllamaInteract.GUI/ViewModels/MainWindowViewModel.cs
+++ src/OllamaInteract.GUI/ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,25 @@
-﻿namespace OllamaInteract.GUI.ViewModels;
+﻿using System.Threading.Tasks;
+using OllamaInteract.Core.Services;
+
+namespace OllamaInteract.GUI.ViewModels;
 
 public partial class MainWindowViewModel : ViewModelBase
 {
-    public string Greeting { get; } = "Welcome to Avalonia!";
+    private readonly IOllamaApiClient _ollamaClient;
+    private readonly IConfigManager _configService;
+    private readonly ServerManager _serverManager;
+
+    public MainWindowViewModel(IOllamaApiClient ollamaClient, IConfigManager configManager, ServerManager serverManager)
+    {
+        _ollamaClient = ollamaClient;
+        _configService = configManager;
+        _serverManager = serverManager;
+
+        _ = InitializeAsync();
+    }
+
+    private async Task InitializeAsync()
+    {
+        
+    }
 }
