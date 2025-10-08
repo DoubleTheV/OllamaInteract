e90cb01637502d6e1c7ca1d6b14295b3cdc5269f
VV <wilczu102@gmail.com>
2025-10-08 09:11:01 +0000

    Make ChatRequest and ChatResponse implement new ChatMessage

--- src/OllamaInteract.Core/Models/ChatResponse.cs
+++
@@ -1,9 +0,0 @@
-namespace OllamaInteract.Core.Models;
-
-public class ChatResponse
-{
-    public bool Success { get; set; }
-    public string Response { get; set; } = string.Empty;
-    public long ResponseTime { get; set; }
-    public string Error { get; set; } = string.Empty;
-}
