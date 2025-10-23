33c674ed0b078cff87479d16f7fb67838898d0d1
VV <wilczu102@gmail.com>
2025-10-23 20:50:21 +0000

    adjusted Conversation model

--- src/OllamaInteract.Core/Models/Conversation.cs
+++ src/OllamaInteract.Core/Models/Conversation.cs
@@ -2,8 +2,17 @@ namespace OllamaInteract.Core.Models;
 
 public class Conversation
 {
-    private  uint? ID { get; set; } = null;
+    public uint? ID { get; private set; } = null;
     public string Name { get; set; } = string.Empty;
     public List<ChatMessage> Messages { get; set; } = new List<ChatMessage>();
-    public string Model { get; set; } = string.Empty;
+
+    public Conversation()
+    {
+        Name = "New conversation";
+    }
+
+    public Conversation(uint? id)
+    {
+        ID = id;
+    }
 }
