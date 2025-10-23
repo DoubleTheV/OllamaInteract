034ec3fe9681fa87cd1b0162e98c8d879ce7e308
VV <wilczu102@gmail.com>
2025-10-23 20:51:00 +0000

    Updated DatabaseManager interface

--- src/OllamaInteract.Core/Services/IDatabaseManager.cs
+++ src/OllamaInteract.Core/Services/IDatabaseManager.cs
@@ -5,6 +5,5 @@ namespace OllamaInteract.Core.Services;
 public interface IDatabaseManager
 {
     List<Conversation> Conversations { get; }
-    List<Conversation> LoadConversations();
     void UpdateConversation(uint conversationID, Action<Conversation> updateAction);
 }
