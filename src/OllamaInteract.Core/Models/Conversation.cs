25cb6d817ae5d5316b4dcb38ae5aa3d164c5cd0c
VV <wilczu102@gmail.com>
2025-10-23 11:56:35 +0000

    made ID an nullable uint

--- src/OllamaInteract.Core/Models/Conversation.cs
+++ src/OllamaInteract.Core/Models/Conversation.cs
@@ -2,7 +2,7 @@ namespace OllamaInteract.Core.Models;
 
 public class Conversation
 {
-    private int? ID { get; set; } = null;
+    private  uint? ID { get; set; } = null;
     public string Name { get; set; } = string.Empty;
     public List<ChatMessage> Messages { get; set; } = new List<ChatMessage>();
     public string Model { get; set; } = string.Empty;
