d50385994f4e90f67d497d01d9551e7ff5b2183e
VV <wilczu102@gmail.com>
2025-10-24 17:39:48 +0000

    removed unneccessary constructor

--- src/OllamaInteract.Core/Models/Conversation.cs
+++ src/OllamaInteract.Core/Models/Conversation.cs
@@ -2,17 +2,13 @@ namespace OllamaInteract.Core.Models;
 
 public class Conversation
 {
-    public uint? ID { get; private set; } = null;
+    public uint ID { get; private set; }
     public string Name { get; set; } = string.Empty;
     public List<ChatMessage> Messages { get; set; } = new List<ChatMessage>();
 
-    public Conversation()
-    {
-        Name = "New conversation";
-    }
-
-    public Conversation(uint? id)
+    public Conversation(uint id)
     {
         ID = id;
+        Name = "New conversation";
     }
 }
