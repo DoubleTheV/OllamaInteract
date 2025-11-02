8d4f5f165c19a8becb48e2a862a8f3947cb9e891
VV <wilczu102@gmail.com>
2025-11-02 12:41:00 +0000

    Change directory to always relative to executable, and fix other minor issues with the driectories

--- src/OllamaInteract.Core/Models/AppConfig.cs
+++ src/OllamaInteract.Core/Models/AppConfig.cs
@@ -8,17 +8,17 @@ public class AppConfig
     public bool PythonRunOnStartup { get; set; } = true;
 
     [JsonPropertyName("python_server_directory")] // relative to .exe // release primary and fallback to production
-    public string[] PythonServerDirectory { get; set; } = {"Python/", "../OllamaInteract.PythonServer/"};
+    public string[] PythonServerDirectory { get; set; } = {"Python/", "../../../../OllamaInteract.PythonServer/"};
 
     [JsonPropertyName("python_executable")]
     public string PythonExectuable { get; set; } = "python";
 
     [JsonPropertyName("python_venv_directory")]
-    public string[] PythonVenvDirectory { get; set; } = {"Python/.venv/", "../../.venv"};
+    public string[] PythonVenvDirectory { get; set; } = {"Python/.venv/", "../../../../../.venv"};
 
 
     [JsonPropertyName("python_requirements_path")] // relative to .exe // release primary and fallback to production
-    public string[] PythonRequirementsDirectory { get; set; } = {"Python/requirements.txt", "../../requirements.txt"};
+    public string[] PythonRequirementsDirectory { get; set; } = {"Python/requirements.txt", "../../../../../requirements.txt"};
 
     [JsonPropertyName("python_server_host")]
     public string PythonHost { get; set; } = "localhost";
